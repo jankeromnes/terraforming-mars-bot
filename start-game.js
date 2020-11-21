247c2f58b3efb4ab1adf17d4b2626f1524dd83d3
Jan Keromnes <janx@linux.com>
2020-11-21 10:26:47 +0000

    Implement 'node start-game'

--- start-game.js
+++ start-game.js
@@ -1,7 +1,9 @@
 // Copyright © 2020 Jan Keromnes.
 // The following code is covered by the MIT license.
 
+const fs = require('fs');
 const minimist = require('minimist');
+const request = require('./lib/request');
 
 const usage = `Usage: node start-game [SERVER]`;
 const argv = minimist(process.argv.slice(2));
@@ -13,3 +15,10 @@ if (argv.help || argv._.length > 1) {
 
 const serverUrl = argv._[0] || 'http://localhost:8080';
 console.log('Server URL:', serverUrl);
+
+const settings = JSON.parse(fs.readFileSync('./assets/tm_settings_solo_game.json', 'utf-8'));
+
+(async () => {
+  const data = await request('PUT', `${serverUrl}/game`, settings);
+  console.log('Started new game:', data);
+})();
